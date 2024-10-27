d1909924b07fe7cd43a9422669632a10610b8a28
Barbora6 <145871155+Barbora6@users.noreply.github.com>
2024-10-27 22:04:24 +0100

    pages

---
+++ Home.js
@@ -0,0 +1,9 @@
+import './Home.css'
+
+const Home = () => {
+  return <section className='home-page'>
+    <h1>Úvodní strana</h1>
+  </section>
+}
+
+export default Home
---
+++ Serials.js
@@ -0,0 +1,9 @@
+import './Serials.css'
+
+const Serials = () => {
+  return <section className='serials-page'>
+   <p>Obsah stránky Serials</p>
+  </section>
+}
+
+export default Serials
